cb5cf6bab3107a6ccd6361c656128b16c31acf01
Ryan <jh3155@gmail.com>
2019-01-10 19:51:40 -0500

    adding formatDouble method

--- sellit/src/main/java/com/sellit/util/DoubleUtil.java
+++ sellit/src/main/java/com/sellit/util/DoubleUtil.java
@@ -1,9 +1,17 @@
 package com.sellit.util;
 
+import java.text.DecimalFormat;
+
 public class DoubleUtil {
+
+	private static final DecimalFormat decimalFormat = new DecimalFormat("#.00");
 
 	public static Double truncate(Double value) {
 		return Math.round(value * 100.0) / 100.0;
+	}
+
+	public static String formatDouble(Double value) {
+		return decimalFormat.format(value);
 	}
 
 }
